3f8d100c7e3c0ef65ea2cb7cceb5f2e830a7b831
Cirus Thenter <cirusthenter@gmail.com>
2021-06-07 02:38:43 +0900

    modified sorting order from ascending to descending

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
 #include "functions.hpp"
 
-int main(int argc, char* argv[])
+void error_rate(string inpath, string outpath)
 {
-    Graph g = read_graph(argv[1]);
-    ofstream output(argv[2]);
+
+    Graph g = read_graph(inpath);
+    ofstream output(outpath);
 
     auto start1 = chrono::high_resolution_clock::now();
     vector<double> bc = g.compute_bc();
@@ -13,7 +14,7 @@ int main(int argc, char* argv[])
     for (int i = 0; i < bc.size(); ++i) {
         bc_index.push_back(make_pair(bc[i], i));
     }
-    sort(bc_index.begin(), bc_index.end());
+    sort(bc_index.begin(), bc_index.end(), greater<pair<double, int>>());
     cout << "exact bc: " << (double)duration1.count() / 1000000 << " sec" << endl;
     print_sum_vectror(bc);
     cout << "diameter = " << g.get_diameter() << endl;
@@ -27,7 +28,7 @@ int main(int argc, char* argv[])
         for (int i = 0; i < bc.size(); ++i) {
             limited_bc_index.push_back(make_pair(limited_bc[i], i));
         }
-        sort(limited_bc_index.begin(), limited_bc_index.end());
+        sort(limited_bc_index.begin(), limited_bc_index.end(), greater<pair<double, int>>());
         cout << "limit = " << limit << endl;
         output << limit << " ";
         double time = (double)duration2.count() / 1000000;
@@ -41,5 +42,35 @@ int main(int argc, char* argv[])
         output << endl;
         print_sum_vectror(limited_bc);
     }
+}
+
+void top10(string inpath)
+{
+
+    Graph g = read_graph(inpath);
+
+    vector<double> bc = g.compute_bc();
+    vector<pair<double, int>> bc_index;
+    for (int i = 0; i < bc.size(); ++i) {
+        bc_index.push_back(make_pair(bc[i], i));
+    }
+    sort(bc_index.begin(), bc_index.end(), greater<pair<double, int>>());
+
+    vector<double> limited_bc = g.compute_limited_bc(2);
+    vector<pair<double, int>> limited_bc_index;
+    for (int i = 0; i < bc.size(); ++i) {
+        limited_bc_index.push_back(make_pair(limited_bc[i], i));
+    }
+    sort(limited_bc_index.begin(), limited_bc_index.end(), greater<pair<double, int>>());
+
+    for (int i = 0; i < bc_index.size(); ++i) {
+        cout << "|" << i + 1 << "|" << bc_index[i].second << "|" << limited_bc_index[i].second << "|" << bc_index[i].first << "|" << limited_bc_index[i].first << "|" << endl;
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    // error_rate(argv[1], argv[2]);
+    top10(argv[1]);
     return 0;
 }
